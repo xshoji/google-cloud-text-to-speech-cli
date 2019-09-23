0cca647eca79cf40468a342508705d869f868cca
xshoji <xshoji@users.noreply.github.com>
2019-09-23 22:08:35 +0900

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# google-cloud-text-to-speech-cli
+## google-cloud-text-to-speech-cli
+
 Command line interface for google-cloud-text-to-speech-api.
+
+> Cloud Text-to-Speech Client Libraries  ｜  Cloud Text-to-Speech API  ｜  Google Cloud  
+> https://cloud.google.com/text-to-speech/docs/reference/libraries?hl=ja
+
+## How to use
+
+#### Export environment value
+
+```
+export GOOGLE_APPLICATION_CREDENTIALS=/path/to/credential.json
+```
+
+#### Build and run
+
+```
+go build .
+```
+
+## Development
+
+#### `go get` google libraries
+
+```
+go get -u cloud.google.com/go/texttospeech/apiv1
+go get -u github.com/googleapis/google-cloud-go
+```
+
+> googleapis/google-cloud-go： Google Cloud Client Libraries for Go.  
+> https://github.com/googleapis/google-cloud-go
+
+#### Create `Service Account Key`
+
+> Cloud Text-to-Speech Client Libraries  ｜  Cloud Text-to-Speech API  ｜  Google Cloud  
+> https://cloud.google.com/text-to-speech/docs/reference/libraries?hl=ja
+
+Then, download credential json.
+
+```
+2019/09/23 20:00:08 rpc error: code = PermissionDenied desc = Cloud Text-to-Speech API has not been used in project xxxxxx before or it is disabled. Enable it by visiting https://console.cloud.google.com/apis/api/texttospeech.googleapis.com/overview?project=xxxxx then retry. If you enabled this API recently, wait a few minutes for the action to propagate to our systems and retry.
+```
+
+And enable API account ( register your credit card info ).
+
+## References
+
+> サポートされているすべての音声の一覧表示  ｜  Cloud Text-to-Speech Documentation  ｜  Google Cloud  
+> https://cloud.google.com/text-to-speech/docs/list-voices?hl=ja
---
+++ main.go
@@ -0,0 +1,163 @@
+// Command quickstart generates an audio file with the content "Hello, World!".
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/jessevdk/go-flags"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+
+	"cloud.google.com/go/texttospeech/apiv1"
+	texttospeechpb "google.golang.org/genproto/googleapis/cloud/texttospeech/v1"
+)
+
+const AppName = "google-cloud-text-to-speech-cli"
+
+type options struct {
+	Text           string  `short:"t" long:"text" description:"[required] Text content." default:""`
+	LanguageCode   string  `short:"l" long:"language" description:"LanguageCode." default:"en"`
+	Gender         string  `short:"g" long:"gender" description:"SsmlGender." default:"FEMALE"`
+	Voice          string  `short:"v" long:"voice" description:"Voice type ( see --listvoicetype )." default:"en-AU-Standard-A"`
+	SpeakingRate   float64 `short:"s" long:"rate" description:"SpeakingRate." default:"1.0"`
+	Pitch          float64 `short:"p" long:"pitch" description:"Pitch." default:"1.0"`
+	OutputFilePath string  `short:"o" long:"output" description:"Output file path." default:"output.mp3"`
+	ListVoiceType  bool    `long:"listvoicetype" description:"Display voice types."`
+}
+
+func main() {
+
+	// [ --help and error help ]
+	opts := *new(options)
+	parser := flags.NewParser(&opts, flags.Default)
+	// set name
+	parser.Name = AppName
+	if _, err := parser.Parse(); err != nil {
+		flagsError, _ := err.(*flags.Error)
+		// help時は何もしない
+		if flagsError.Type == flags.ErrHelp {
+			return
+		}
+		fmt.Println()
+		parser.WriteHelp(os.Stdout)
+		fmt.Println()
+		return
+	}
+
+	// Required parameter
+	// - [Can Go's `flag` package print usage? - Stack Overflow](https://stackoverflow.com/questions/23725924/can-gos-flag-package-print-usage)
+	fmt.Println("text: ", opts.Text)
+	fmt.Println("language: ", opts.LanguageCode)
+	fmt.Println("gender: ", opts.Gender)
+	fmt.Println("speakingRate: ", opts.SpeakingRate)
+	fmt.Println("pitch: ", opts.Pitch)
+	fmt.Println("output: ", opts.OutputFilePath)
+	fmt.Println()
+
+	// list mode
+	if opts.ListVoiceType {
+		ListVoices(os.Stdout, opts.LanguageCode)
+		os.Exit(0)
+	}
+
+	if opts.Text == "" {
+		log.Fatal("text is empty.")
+	}
+	ssmlVoiceGender, ok := texttospeechpb.SsmlVoiceGender_value[opts.Gender]
+	if !ok {
+		log.Fatal("Undefined voice type.")
+	}
+
+	// Instantiates a client.
+	ctx := context.Background()
+
+	client, err := texttospeech.NewClient(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Perform the text-to-speech request on the text input with the selected
+	// voice parameters and audio file type.
+	req := texttospeechpb.SynthesizeSpeechRequest{
+		// Set the text input to be synthesized.
+		Input: &texttospeechpb.SynthesisInput{
+			InputSource: &texttospeechpb.SynthesisInput_Text{Text: opts.Text},
+		},
+		// Build the voice request, select the language code ("en-US") and the SSML
+		// voice genderFlag ("neutral").
+		Voice: &texttospeechpb.VoiceSelectionParams{
+			LanguageCode: opts.LanguageCode,
+			SsmlGender:   texttospeechpb.SsmlVoiceGender(ssmlVoiceGender),
+		},
+		// Select the type of audio file you want returned.
+		AudioConfig: &texttospeechpb.AudioConfig{
+			AudioEncoding: texttospeechpb.AudioEncoding_MP3,
+			SpeakingRate:  1.0,
+			Pitch:         1.0,
+		},
+	}
+
+	resp, err := client.SynthesizeSpeech(ctx, &req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// The resp's AudioContent is binary.
+	err = ioutil.WriteFile(opts.OutputFilePath, resp.AudioContent, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Audio content written to file: %v\n", opts.OutputFilePath)
+}
+
+// > サポートされているすべての音声の一覧表示  ｜  Cloud Text-to-Speech Documentation  ｜  Google Cloud
+// > https://cloud.google.com/text-to-speech/docs/list-voices?hl=ja
+// ListVoices lists the available text to speech voices.
+func ListVoices(w io.Writer, language string) error {
+	ctx := context.Background()
+
+	client, err := texttospeech.NewClient(ctx)
+	if err != nil {
+		return err
+	}
+
+	// Performs the list voices request.
+	resp, err := client.ListVoices(ctx, &texttospeechpb.ListVoicesRequest{})
+	if err != nil {
+		return err
+	}
+
+	for _, voice := range resp.Voices {
+
+		// Display the supported language codes for this voice. Example: "en-US"
+		isSupported := false
+		for _, languageCode := range voice.LanguageCodes {
+			if strings.HasPrefix(languageCode, language) {
+				isSupported = true
+			}
+		}
+		if !isSupported {
+			continue
+		}
+
+		// Display the voice's name. Example: tpc-vocoded
+		fmt.Fprintf(w, "Name: %v\n", voice.Name)
+
+		// Display the supported language codes for this voice. Example: "en-US"
+		for _, languageCode := range voice.LanguageCodes {
+			fmt.Fprintf(w, "  Supported language: %v\n", languageCode)
+		}
+
+		// Display the SSML Voice Gender.
+		fmt.Fprintf(w, "  SSML Voice Gender: %v\n", voice.SsmlGender.String())
+
+		// Display the natural sample rate hertz for this voice. Example: 24000
+		fmt.Fprintf(w, "  Natural Sample Rate Hertz: %v\n",
+			voice.NaturalSampleRateHertz)
+	}
+
+	return nil
+}
